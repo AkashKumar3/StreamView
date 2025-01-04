0190bb0c4f1448630fcad6ebd894864380100bdd
AkashKumar3 <akashkumar8862825283@gmail.com>
2025-01-04 18:55:22 +0530

    added signup and signin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Home from './Pages/Home';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
-      <Router>
-      <div className="h-screen">
-      <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
-      <Sidebar isOpen={isSidebarOpen} />
-      <main className={`pt-14 ${isSidebarOpen ? 'ml-64' : ''} transition-margin duration-300 ease-in-out`}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-        </div>
-      </Router>
-  )
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="*"
+          element={
+            <div className="h-screen">
+              <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+              <Sidebar isOpen={isSidebarOpen} />
+              <main
+                className={`pt-14 ${
+                  isSidebarOpen ? "ml-64" : ""
+                } transition-margin duration-300 ease-in-out`}
+              >
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                </Routes>
+              </main>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
