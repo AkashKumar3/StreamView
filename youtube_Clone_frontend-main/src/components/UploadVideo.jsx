9030154ef0a424cd7767a6515f721015e854f122
AkashKumar3 <87637130+AkashKumar3@users.noreply.github.com>
2025-01-18 12:09:00 +0530

    Update UploadVideo.jsx

--- youtube_Clone_frontend-main/src/components/UploadVideo.jsx
+++ youtube_Clone_frontend-main/src/components/UploadVideo.jsx
@@ -43,7 +43,7 @@ const UploadVideo = () => {
 
     try {
       let result = await axios.post(
-        "http://localhost:5000/api/video/addVideo",
+        "https://youtube-clone-backend-ghq6.onrender.com/api/video/addVideo",
         videoData,
         {
           headers: {
